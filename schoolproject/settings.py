13603e499666ffadd7cb917013b207f538ec45c7
brightgeorge <98938996+brightgeorge@users.noreply.github.com>
2023-03-15 11:29:33 +0530

    Update settings.py

--- schoolproject/settings.py
+++ schoolproject/settings.py
@@ -78,11 +78,8 @@ WSGI_APPLICATION = 'schoolproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'atholi',
-        'USER':'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
